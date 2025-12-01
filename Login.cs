dc3d9c4e9d51504769718da14523320eb8e690dd
Phoebe Kaye Pontiñoza <pontinozaphoebekaye_bsit@plmun.edu.ph>
2025-12-02 00:53:26 +0800

    Create Login.cs

---
+++ Login.cs
@@ -0,0 +1,47 @@
+Login.cs
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryAndSales
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "admin" && txtPassword.Text == "12345")
+            {
+                MessageBox.Show("You have successfully logged in", "Meesage", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MainMenu mm = new MainMenu();
+                mm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
